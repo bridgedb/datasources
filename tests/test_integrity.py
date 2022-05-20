a6f9603ea566449dd472bcf2e4aabbd47a4429a4
Charles Tapley Hoyt <cthoyt@gmail.com>
2022-05-20 13:26:36 +0200

    Fix trailing whitespace in lines

--- tests/test_integrity.py
+++ tests/test_integrity.py
@@ -9,23 +9,36 @@ ROOT = HERE.parent.resolve()
 HEADERS = ROOT.joinpath("headers.tsv")
 DATASOURCES = ROOT.joinpath("datasources.tsv")
 
+#: The number of columns. Change if more columns are added.
+N_COLUMNS = 12
+
 
 class TestIntegrity(unittest.TestCase):
     """Test data integrity."""
 
+    def setUp(self) -> None:
+        """Load the data sources rows."""
+        with DATASOURCES.open() as file:
+            self.rows = list(csv.reader(file, delimiter="\t"))
+
+    def test_lengths(self):
+        """Test the row lengths."""
+        for i, line in enumerate(self.rows, start=1):
+            self.assertEqual(
+                N_COLUMNS, len(line), msg=f"Row {i} has the wrong number of columns"
+            )
+
     def test_valid_miriam(self):
         """Test that MIRIAM prefixes are valid."""
         valid_miriam_prefixes = set(get_miriam())
-        with DATASOURCES.open() as file:
-            reader = csv.reader(file, delimiter='\t')
-            for i, line in enumerate(reader):
-                resource, uri = line[0], line[8]
-                if not uri.startswith("urn:miriam"):
-                    continue
-                miriam_prefix = uri.removeprefix("urn:miriam:")
-                with self.subTest(resource=resource, miriam=miriam_prefix):
-                    self.assertIn(
-                        miriam_prefix,
-                        valid_miriam_prefixes,
-                        msg=f"\n\t[line {i}] Invalid MIRIAM prefix for {resource}: {uri}",
-                    )
+        for i, line in enumerate(self.rows, start=1):
+            resource, uri = line[0], line[8]
+            if not uri.startswith("urn:miriam"):
+                continue
+            miriam_prefix = uri.removeprefix("urn:miriam:")
+            with self.subTest(resource=resource, miriam=miriam_prefix):
+                self.assertIn(
+                    miriam_prefix,
+                    valid_miriam_prefixes,
+                    msg=f"\n\t[line {i}] Invalid MIRIAM prefix for {resource}: {uri}",
+                )
